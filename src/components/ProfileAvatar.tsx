610eaabc144ab57e4359bddbfb96ec1151481de7
janeiaexpert <janeiaexpert@users.noreply.github.com>
2026-08-29 23:12:25 -0300

    fix: avatar sizes sm=64 md=112 lg=144

--- src/components/ProfileAvatar.tsx
+++ src/components/ProfileAvatar.tsx
@@ -7,8 +7,8 @@ interface ProfileAvatarProps {
 
 const sizes = {
   sm: "w-16 h-16",
-  md: "w-24 h-24",
-  lg: "w-24 h-24 sm:w-32 sm:h-32",
+  md: "w-28 h-28",
+  lg: "w-36 h-36",
 };
 
 const ProfileAvatar = ({ size = "lg", onClick, className = "", src }: ProfileAvatarProps) => {
